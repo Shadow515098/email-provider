5afd6172596fd6d457e2b441672606c9575bf4bf
Shadow515098 <shadowgamer515098@gmail.com>
2024-03-20 08:32:23 +0100

    first commit

---
+++ email-provider/src/main/java/de/email_provider/App.java
@@ -0,0 +1,13 @@
+package de.email_provider;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
